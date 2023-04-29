12f06f254e4a37b72cd88f977b37884785223b7a
Kurogoma <gogottt2009-hp@yahoo.co.jp>
2023-04-29 14:55:45 +0900

    Extract calculation routine

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,15 @@
 use std::error::Error;
 
-pub fn run() -> Result<(), Box<dyn Error>> {
-    const IMAGE_WIDTH: u32 = 256;
-    const IMAGE_HEIGHT: u32 = 256;
+const IMAGE_WIDTH: u32 = 256;
+const IMAGE_HEIGHT: u32 = 256;
 
+pub fn run() -> Result<(), Box<dyn Error>> {
     println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
 
     for j in (0..IMAGE_HEIGHT).rev() {
         eprint!("\rScanlines remaining: {} ", j);
         for i in 0..IMAGE_WIDTH {
-            let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
-            let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
-            let b = 0.25f64;
+            let (r, g, b) = calculate_color(i, j);
 
             let ir = (255.999 * r) as u32;
             let ig = (255.999 * g) as u32;
@@ -22,4 +20,11 @@ pub fn run() -> Result<(), Box<dyn Error>> {
     }
     eprintln!("\nDone.");
     Ok(())
+}
+
+fn calculate_color(i: u32, j: u32) -> (f64, f64, f64) {
+    let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
+    let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
+    let b = 0.25f64;
+    (r, g, b)
 }
