62d90058f7946be8fe58f5db067c1b11f7821ec0
Kurogoma <gogottt2009-hp@yahoo.co.jp>
2023-04-29 16:36:10 +0900

    Render surface normals on a sphere

--- src/ray.rs
+++ src/ray.rs
@@ -16,20 +16,26 @@ impl Ray {
     }
 
     pub fn color(&self) -> Vec3 {
-        if self.hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5) {
-            return Vec3::new(1.0, 0.0, 0.0);
+        let t = self.hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5);
+        if t > 0.0 {
+            let n = (self.at(t) - Vec3::new(0.0, 0.0, -1.0)).unit();
+            return 0.5 * Vec3::new(n.x + 1.0, n.y + 1.0, n.z + 1.0);
         }
         let unit = self.direction.unit();
         let t = 0.5 * (unit.y + 1.0);
         (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
     }
 
-    fn hit_sphere(&self, center: Vec3, radius: f64) -> bool {
+    fn hit_sphere(&self, center: Vec3, radius: f64) -> f64 {
         let oc = self.origin - center;
         let a = self.direction.dot(&self.direction);
         let b = 2.0 * oc.dot(&self.direction);
         let c = oc.dot(&oc) - radius * radius;
         let discriminant = b * b - 4.0 * a * c;
-        discriminant > 0.0
+        if discriminant < 0.0 {
+            -1.0
+        } else {
+            (-b - discriminant.sqrt()) / (2.0 * a)
+        }
     }
 }
