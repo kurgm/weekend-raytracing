16a9c9d89e61fc0de5b7d2ac68b5c7a01650dbed
Kurogoma <gogottt2009-hp@yahoo.co.jp>
2023-04-29 16:28:18 +0900

    Render a red sphere

--- src/ray.rs
+++ src/ray.rs
@@ -16,8 +16,20 @@ impl Ray {
     }
 
     pub fn color(&self) -> Vec3 {
+        if self.hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5) {
+            return Vec3::new(1.0, 0.0, 0.0);
+        }
         let unit = self.direction.unit();
         let t = 0.5 * (unit.y + 1.0);
         (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
+    }
+
+    fn hit_sphere(&self, center: Vec3, radius: f64) -> bool {
+        let oc = self.origin - center;
+        let a = self.direction.dot(&self.direction);
+        let b = 2.0 * oc.dot(&self.direction);
+        let c = oc.dot(&oc) - radius * radius;
+        let discriminant = b * b - 4.0 * a * c;
+        discriminant > 0.0
     }
 }
